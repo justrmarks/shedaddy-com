43232c0c0077e43423379729459d1a63eabe313f
justrmarks <blackbirdwanderer@gmail.com>
2020-02-06 11:06:53 -0800

    fix: implemented Error Boundry component and updated suspense fallback

--- src/components/ThreeJS/canvasJumbotron.jsx
+++ src/components/ThreeJS/canvasJumbotron.jsx
@@ -1,19 +1,19 @@
-import React, { useRef, useState } from 'react'
-import { Canvas, useFrame } from 'react-three-fiber'
+import React from 'react'
+import { Canvas } from 'react-three-fiber'
 import Snapdragon from './snapDragon'
 
 
 
 // component to handle error detection and canvas rendering for 
 // react Three Fiber canvas component
-export const CanvasJumbotron = (props)=> {
-
-    if ()
+const CanvasJumbotron = (props)=> {
     return (
-    <Canvas>
+    <Canvas {...props}>
         <ambientLight />
         <pointLight position={[10, 10, 10]} />
         <Snapdragon position={[-1.2, 0, 0]} />
         <Snapdragon position={[1.2, 0, 0]} />
+        <Snapdragon position={[1.2, 1, -3]} />
     </Canvas> )
 }
+export default CanvasJumbotron;
